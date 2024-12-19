41cdffe68e5b2c31c2508577d26e84ee518fa1e3
ELipovac <emili.lipovac@student.uniri.hr>
2024-12-19 15:59:56 +0100

    Add new files and folders

--- pets/views.py
+++ pets/views.py
@@ -1,89 +1,47 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.views.generic import ListView, DetailView
-from .models import Pet
-from .models import VeterinaryService
-from datetime import datetime
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+from .models import  Pet, VeterinaryService
 
-# View for user registration
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to the login page after successful registration
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+def homepage(request):
+    return HttpResponse('<strong>Kućni ljubimci i veterinarske usluge</strong>')
 
-# View for managing users, restricted to superusers
-@login_required
-@user_passes_test(lambda user: user.is_superuser)
-def manage_users(request):
-    users = User.objects.all()
-    return render(request, 'manage_users.html', {'users': users})
-
-# View restricted to logged-in users
-@login_required
-def restricted_view(request):
-    return render(request, 'restricted.html')
-
-# Function to check if the user is an admin (superuser)
-def is_admin(user):
-    return user.is_superuser
-
-# View restricted to admin (superuser) users
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_only.html')
-
-# ListView for displaying all pets
 class PetListView(ListView):
     model = Pet
-    template_name = 'pet_management_app/pet_list.html'  # Path to the template
-    context_object_name = 'pets'
-    paginate_by = 10  # Paginate results
+    template_name = './pet_list.html'  
+    context_object_name = 'objects'    
+    paginate_by = 10                   
 
     def get_queryset(self):
-        queryset = Pet.objects.all()
-
-        # Filter by user if specified in GET request
-        user = self.request.GET.get('user')
-        if user:
-            queryset = queryset.filter(owner__username=user)
-
-        # Filter by creation date if specified in GET request
-        created_after = self.request.GET.get('created_after')
-        if created_after:
-            queryset = queryset.filter(created__gte=created_after)
-
+        queryset = super().get_queryset()
+        filter_param = self.request.GET.get('filter', None)
+        if filter_param:
+            queryset = queryset.filter(your_field__icontains=filter_param)  # Filtriraj prema potrebi
         return queryset
 
-# DetailView for displaying the details of a specific pet
-class PetDetailView(DetailView):
-    model = Pet
-    template_name = 'pet_management_app/pet_detail.html'  # Path to the template
-    context_object_name = 'pet'
 
-
-# ListView za prikaz svih veterinarskih usluga
 class VeterinaryServiceListView(ListView):
     model = VeterinaryService
-    template_name = 'vet_service_list.html'  # Predložak koji ćemo kasnije napraviti
-    context_object_name = 'vet_services'
+    template_name = './veterinaryservice_list.html'  
+    context_object_name = 'objects'    
+    paginate_by = 10                   
 
     def get_queryset(self):
-        # Filtriranje prema vrsti usluge
-        queryset = VeterinaryService.objects.all()
-        service_query = self.request.GET.get('service', '')
-        if service_query:
-            queryset = queryset.filter(service_type__icontains=service_query)  # Pretraživanje prema vrsti usluge
+        queryset = super().get_queryset()
+        filter_param = self.request.GET.get('filter', None)
+        if filter_param:
+            queryset = queryset.filter(your_field__icontains=filter_param) 
         return queryset
 
-# DetailView za veterinarske usluge
+
+class PetDetailView(DetailView):
+    model = Pet
+    template_name = 'pet_detail.html'
+    context_object_name = 'object'           
+
+
 class VeterinaryServiceDetailView(DetailView):
     model = VeterinaryService
-    template_name = 'vet_service_detail.html'  # Predložak koji ćemo kasnije napraviti
-    context_object_name = 'vet_service'
+    template_name = 'veterinaryservice_detail.html'
+    context_object_name = 'object'          
